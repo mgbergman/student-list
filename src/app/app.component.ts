69196b1569054f908e09db58af9b4f5fdfa0f249
Marc Bergman <mgbergman@gmail.com>
2020-11-10 11:43:21 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  students: any [] = [];
+  studentCollection: any[] = [
+    { name: "Tonya", favNumber: 743999, favColor: "orange"},
+    { name: "Kalil", favNumber: 743999, favColor: "orange"},
+    { name: "Marc", favNumber: 743999, favColor: "orange"},
+    { name: "Tina", favNumber: 743999, favColor: "orange"},
+    { name: "Bruce", favNumber: 743999, favColor: "orange"},
+    { name: "Oliver", favNumber: 743999, favColor: "orange"},
+    { name: "Zoe", favNumber: 743999, favColor: "orange"},
+  ];
   title = 'student-list';
+
+  getStudents(): void {
+
+    this.students = this.studentCollection;
+  
+  }
 }
